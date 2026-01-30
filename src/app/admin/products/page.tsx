622ff6d1dee37798b505a19314a7dab6693f920f
Arturo Gutierrez <agutierrez.gyo@gmail.com>
2026-01-30 11:53:38 -0300

    v.0.01

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,16 +1,3 @@
-// 
-// import ProductsClient from '@/src/app/admin/products/ProductsClient';
-// import { getMadreProducts } from '@/src/service/madreProducts.read';
-// 
-// export default async function ProductsPage() {
-//   const data = await getMadreProducts({
-//     offset: 0,
-//     limit: 50,
-//   });
-// 
-//   return (
-//     <ProductsClient
-//       initialData={data}
-//     />
-//   );
-// }
+export default function Page() {
+  return <div>Orders</div>;
+}
